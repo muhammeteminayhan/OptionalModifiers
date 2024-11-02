5fc805b5168add978c843567d867560003158176
Muhammet Emin Ayhan <aemin8343@gmail.com>
2024-11-02 18:50:53 +0300

    First Commit

--- OptionalModifiers/ContentView.swift
+++ OptionalModifiers/ContentView.swift
@@ -7,15 +7,49 @@
 
 import SwiftUI
 
+enum Effect: String, CaseIterable {
+    case bounce = "Bounce"
+    case breathe = "Breathe"
+    case pulse = "Pulse"
+    case rotate = "Rotate"
+}
+
 struct ContentView: View {
+    @State private var effect: Effect = .bounce
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Group {
+            Picker("", selection: $effect) {
+                ForEach(Effect.allCases,id: \.rawValue) {
+                    Text($0.rawValue)
+                        .tag($0)
+                }
+            }
+            .pickerStyle(.segmented)
+            .padding(15)
+            
+            Image(systemName: "heart.fill")
+                .font(.largeTitle)
+                .foregroundStyle(.red.gradient)
+                .modifiers { image in
+                    switch effect {
+                    case .bounce:
+                        image.symbolEffect(.bounce)
+                    case .breathe:
+                        image.symbolEffect(.breathe)
+                    case .pulse:
+                        image.symbolEffect(.pulse)
+                    case .rotate:
+                        image.symbolEffect(.rotate)
+                    }
+                }
         }
-        .padding()
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func modifiers<Content: View>(@ViewBuilder content: @escaping (Self) -> Content) -> some View {
+        content(self)
     }
 }
 
