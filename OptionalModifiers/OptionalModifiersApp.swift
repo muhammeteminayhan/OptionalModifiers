bc87f3b73a32fc11c7bde527e76e0824cfa08475
Muhammet Emin Ayhan <aemin8343@gmail.com>
2024-11-02 18:39:33 +0300

    Initial Commit

---
+++ OptionalModifiers/OptionalModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptionalModifiersApp.swift
+//  OptionalModifiers
+//
+//  Created by Muhammet Emin Ayhan on 2.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct OptionalModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
